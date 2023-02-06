ded13c1abfa1d73d3e86e5a10dac1197de18cf21
rezasoumi <reza.soumi80@gmail.com>
2023-02-06 20:52:35 +0330

    connected to bank.

--- express/index.js
+++ express/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import pg from "pg";
 import bodyParser from "body-parser";
-import require from "request"
+import fetch from "node-fetch";
 
 const { Client } = pg;
 
@@ -37,60 +37,50 @@ const main = async () => {
 
   app.post("/transaction", async (req, res) => {
     const { body } = req;
-    var request = require('request');
+    console.log(body);
+    
+    const bodyBankTransaction = {receipt_id: 123, amount: 100, callback: "http://localhost:8080/TransactionCompleted"};
 
-    request.post(
-      //First parameter API to make post request
-      'http://127.0.0.1:8000/transaction/',
-  
-      //Second parameter DATA which has to be sent to API
-      { json: {
-          receipt_id: 123,
-          amount: body.amount
-        } 
-      },
-      
-      //Thrid parameter Callack function  
-      function (error, response, body) {
-          console.log(body);
-          console.log(response);
-          if (!error && response.statusCode == 201) {
-              console.log(body);
-          }
-      }
-    );
+    const response = await fetch('http://localhost:8000/transaction/', {
+      method: 'post',
+      body: JSON.stringify(bodyBankTransaction),
+      headers: {'Content-Type': 'application/json'}
+    });
+    const data = await response.json();
 
-    res.status(200);
-    res.send('Success');
+    console.log(data);
+    var url = "http://localhost:8000/payment/" + data.id;
+    const out = {url: url};
+    //res.status(200);
+    res.send(out);
   });
 
-
-  app.post("/purchase", async (req, res) => {
+  app.post("/getAllPurchases", async (req, res) => {
     const r = await client.query(
-      `INSERT INTO purchase (corresponding_user_id, title, first_name, last_name, flight_serial, offer_price, offer_class, transaction_id, transaction_result)
-      VALUES ($1, $2, $3, $4, $5, $6, $7)`,
-      [body.from, body.to]
+      `
+      SELECT * FROM purchase WHERE corresponding_user_id == $1
+      `,
+      [body.corresponding_user_id]
     );
+
     res.send(JSON.stringify(r.rows));
   });
 
-  app.post("/search", async (req, res) => {
+  
+  app.post("/purchase", async (req, res) => {
     const { body } = req;
-    console.log(body);
+
     const r = await client.query(
-      `
-      SELECT f.*, c1.city AS source_city, c2.city AS dest_city
-      FROM flight f
-      JOIN origin_destination c1 ON f.origin = c1.iata
-      JOIN origin_destination c2 ON f.destination = c2.iata
-      WHERE c1.city = $1 AND c2.city = $2
-      `,
-      [body.from, body.to]
+      `INSERT INTO purchase (corresponding_user_id, title, first_name, last_name, flight_serial, offer_price, offer_class, transaction_id, transaction_result)
+      VALUES ($1, $2, $3, $4, $5, $6, $7, $8, &9)`,
+      [body.corresponding_user_id, body.title, body.first_name, body.last_name, body.flight_serial, body.offer_price, body.offer_class, body.transaction_id, body.transaction_result]
     );
+
     res.send(JSON.stringify(r.rows));
   });
 
   app.listen(12345);
 };
+
 
 main();
