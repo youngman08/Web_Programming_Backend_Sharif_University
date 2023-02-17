c78135b07e6b1ba8f5b116ed593a2bf5b3a5ebc3
rezasoumi <reza.soumi80@gmail.com>
2023-02-17 19:15:57 +0330

    locust updated. not completed.

--- locust/load_test.py
+++ locust/load_test.py
@@ -6,42 +6,39 @@ hash_to_string = {}
 hashes = []
 
 class WebsiteUser(HttpUser):
-    # host = "http://213.233.179.83"
     host = "http://localhost"
 
-
-    def get_go_node(self) -> str:
-        if random.random() < 0.5:
-            return 'go'
-        return 'node'
-  
+    # def get_go_node(self) -> str:
+    #     if random.random() < 0.5:
+    #         return 'go'
+    #     return 'node'
+    
+    # main page
     @task(1)
-    def post_less_than_eight(self):
+    def get_mainpage(self):
         string = str(uuid.uuid1())[:random.randint(0, 7)]
-        result = self.client.post(f"/{self.get_go_node()}/sha256?string={string}").json()
+        result = self.client.post(f"/8000").json()
         assert result['status'] == False
 
-    
+    # sign up
     @task(19)
-    def post_string_ok(self):
-        string = str(uuid.uuid1())
-        result = self.client.post(f"/{self.get_go_node()}/sha256?string={string}").json()
-        assert result['status'] == True
-        hash_to_string[result['sha256']] = string
-        hashes.append(result['sha256'])
+    def post_signup(self):
+        result = self.client.post(f"/8000/signup").json()
+        # assert result['status'] == True
         
+    # login
     @task(10)
-    def get_in_db(self):
-        if not len(hashes):
-            return
-        hsh = hashes[random.randint(0, len(hashes)-1)]
-        result = self.client.get(f"/{self.get_go_node()}/sha256?sha256={hsh}").json()
-        assert result['found'] == True
-        string = result['string']
-        assert string == hash_to_string[hsh]
+    def post_login(self):
+        result = self.client.post(f"/8000/login").json()
+        # assert result['found'] == True
+        # string = result['string']
 
+    # search
     @task(10)
-    def get_not_in_db(self):
-        sha256 = random.getrandbits(256)
-        string = str(uuid.uuid1())
-        result = self.client.get(f"/{self.get_go_node()}/sha256?sha256=%032x" % sha256).json()
+    def get_dashboard(self):
+        result = self.client.get(f"/8001/search").json()
+
+    # dashboard
+    @task(10)
+    def get_dashboard(self):
+        result = self.client.get(f"/8001/dashboard").json()
